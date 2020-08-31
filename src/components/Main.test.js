380dcb9145720ecec77a8327d50371d19142e92d
krishna <krishna@topsinfosolutions.com>
2020-08-31 17:00:02 +0530

    updated readme

--- src/components/Main.test.js
+++ src/components/Main.test.js
@@ -4,7 +4,6 @@ import Adapter from 'enzyme-adapter-react-16';
 // import ReactDOM from 'react-dom';
 import Emitter from '../services/emitter';
 import Main from './Main';
-import Temprature from './Temprature'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -18,18 +17,18 @@ describe('setInterval fn', () => {
         wrapper.instance().airPressureEmit();
         wrapper.instance().humidityEmit();
         // jest.runOnlyPendingTimers();
-        jest.runAllTimers();
+        // jest.runAllTimers();
         let randomNum = (Math.floor(Math.random() * 20) + 1) * 100
-        jest.advanceTimersByTime(randomNum)
+        // jest.advanceTimersByTime(randomNum)
 
-        // let displayObject = {
-        //     temprature: randomNum,
-        //     airPressure: randomNum,
-        //     humidity: randomNum
-        // }
-        // Emitter.emit('INPUT_FROM_TEMPRATURE', displayObject)
-        // Emitter.emit('INPUT_FROM_AIR_PRESSURE', displayObject)
-        // Emitter.emit('INPUT_FROM_HUMIDITY', displayObject)
+        let displayObject = {
+            temprature: randomNum,
+            airPressure: randomNum,
+            humidity: randomNum
+        }
+        Emitter.emit('INPUT_FROM_TEMPRATURE', displayObject)
+        Emitter.emit('INPUT_FROM_AIR_PRESSURE', displayObject)
+        Emitter.emit('INPUT_FROM_HUMIDITY', displayObject)
 
         jest.useRealTimers();
     });
--- src/components/Temprature.test.js
+++ src/components/Temprature.test.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-// import ReactDOM from 'react-dom';
 import Emitter from '../services/emitter';
-// import Main from './Main';
 import Temprature from './Temprature'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('setInterval fn', () => {
     it('set interval and emitter for temprature', () => {
-        // const wrapper = shallow(<Main />);
-
-        // jest.useFakeTimers();
 
         const tempWrapper = shallow(<Temprature />)
         expect(tempWrapper.state("temprature")).toEqual('N/A');
-
-        // wrapper.instance().tempEmit();
-        // let randomNum = (Math.floor(Math.random() * 20) + 1) * 100
-        // jest.advanceTimersByTime(randomNum)
-        // let displayObject = {
-        //     temprature: randomNum
-        // }
-        // Emitter.emit('INPUT_FROM_TEMPRATURE', displayObject)
 
 
         Emitter.on('INPUT_FROM_TEMPRATURE', (newValue) => {
             expect(tempWrapper.state("temprature")).toEqual(`${newValue.temprature}`);
         });
 
-        // jest.useRealTimers();
     });
 })
 
